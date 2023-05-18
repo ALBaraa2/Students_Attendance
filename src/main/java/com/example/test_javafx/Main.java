9479c800362d32e5da12ee5bd4652f8c00171ea3
ALBaraa <116570413+ALBaraa2@users.noreply.github.com>
2023-05-18 12:30:03 +0300

    baraa

--- src/main/java/com/example/test_javafx/Main.java
+++ src/main/java/com/example/test_javafx/Main.java
@@ -15,7 +15,7 @@ public class Main extends Application{
     }
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("views/lecture.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("views/teachassistant.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("University");
         stage.getIcons().add(new Image(Objects.requireNonNull(Main.class.getResourceAsStream("images/uni.jpg"))));
