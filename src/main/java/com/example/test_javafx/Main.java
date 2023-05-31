cc97cb178f5a68fb41df4cd27fcd34121c5ba861
Feras khaled <feras342002@gmail.com>
2023-05-31 14:53:33 +0300

    nothing

--- src/main/java/com/example/test_javafx/Main.java
+++ src/main/java/com/example/test_javafx/Main.java
@@ -8,11 +8,17 @@ import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import java.io.IOException;
 import java.util.Objects;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Main extends Application{
     public static void main(String[] args) {
         launch();
     }
+
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("views/login.fxml"));
